3a9276a21a749a1a6b19d0724f2756a7982e0587
AlphaAmbush <89270361+AlphaAmbush@users.noreply.github.com>
2021-11-30 22:05:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ const double = document.getElementById('double');
 const showMillionairesBtn = document.getElementById('show-millionairs');
 const sort = document.getElementById('sort');
 const calculateWealthBtn = document.getElementById('calculate-wealth');
+const resetBtn = document.getElementById('reset');
 
 let data = [];
+let anotherData =[];
+let countOfMillioner = 1;
 
 async function getRandomUser() {
   const res = await fetch('https://randomuser.me/api');
@@ -36,17 +39,23 @@ function sortByRichest() {
 
 
 function showMillionaires() {
-  data = data.filter((user) => user.money > 1000000);
+  if(countOfMillioner%2!=0){
+    anotherData = data;
+    data = data.filter((user) => user.money > 1000000);
+    showMillionairesBtn.innerHTML ="Show everyone"
+  }else{
+    data = anotherData;
+    showMillionairesBtn.innerHTML ="Show only Millionairs &#x20B9"
+  }
+  countOfMillioner++;
   updateDOM();
 }
 
 
 function calculateWealth() {
   const wealth = data.reduce((acc, user) => (acc += user.money), 0);
-
-  const wealthEl = document.createElement('div');
+  var wealthEl = document.getElementById('wealthEl');
   wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
-  main.appendChild(wealthEl);
 }
 
 function addData(obj) {
@@ -72,11 +81,15 @@ function formatMoney(number) {
   return '₹' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); // 12,345.67
 }
 
+function reset(){
+  location.reload();
+}
 
 addUser.addEventListener('click', getRandomUser);
 double.addEventListener('click', doubleMoney);
 sort.addEventListener('click', sortByRichest);
 showMillionairesBtn.addEventListener('click', showMillionaires);
 calculateWealthBtn.addEventListener('click', calculateWealth);
+resetBtn.addEventListener('click', reset);
 
 
